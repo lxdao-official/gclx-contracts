9b7413e621f2ffe62e53898553f6e68c4b6685be
fourldyy <fourldyy@gmail.com>
2022-04-20 20:34:39 +0800

    refactoring 1. removing the old allowlist function and leaving only merkletree proof; 2. moving validation logics into modifiers ; 3. using ERC721A user AUX data to store allowlist mint numbers instead of mapping

--- test/testsMerkleTree.js
+++ test/testsMerkleTree.js
@@ -27,62 +27,52 @@ describe('contract test - allowList with MerkleProof', function () {
     await hardhatToken.setMerkleRoot(root);
   });
 
-  it('allowlistMintMerkleProof Test #1 - address is in the allowlist ', async function () {
+  it('allowlistMintTest - address is in the allowlist ', async function () {
     allowAccount = allowlistAccounts[0];
     proof = merkleTree.getHexProof(keccak256(allowAccount.address));
-    await hardhatToken
-      .connect(allowAccount)
-      .allowlistMintMerkleProof(2, proof, {
-        value: ethers.utils.parseEther('0.02'),
-      });
+    await hardhatToken.connect(allowAccount).allowlistMint(1, proof, {
+      value: ethers.utils.parseEther('0.01'),
+    });
   });
 
-  it('allowlistMintMerkleProof Test #2 - address is not in the allowlist ', async function () {
+  it('allowlistMintTest #2 - address is not in the allowlist ', async function () {
     proof = merkleTree.getHexProof(keccak256(denyAccount.address));
     try {
       expect(
-        await hardhatToken
-          .connect(denyAccount)
-          .allowlistMintMerkleProof(2, proof, {
-            value: ethers.utils.parseEther('0.02'),
-          })
+        await hardhatToken.connect(denyAccount).allowlistMint(2, proof, {
+          value: ethers.utils.parseEther('0.02'),
+        })
       ).to.throw();
     } catch (e) {
       expect(e.message).to.contain('GCLX: Ni bu zai bai ming dan li');
     }
   });
 
-  it('allowlistMintMerkleProof Test #3 - exceeded max allow list quota (hardcode of 2 in the contract) for this address ', async function () {
+  it('allowlistMintTest #3 - exceeded max allow list quota 2 for this address ', async function () {
     allowAccount = allowlistAccounts[1];
     proof = merkleTree.getHexProof(keccak256(allowAccount.address));
     try {
       expect(
-        await hardhatToken
-          .connect(allowAccount)
-          .allowlistMintMerkleProof(3, proof, {
-            value: ethers.utils.parseEther('0.03'),
-          })
+        await hardhatToken.connect(allowAccount).allowlistMint(3, proof, {
+          value: ethers.utils.parseEther('0.03'),
+        })
       ).to.throw();
     } catch (e) {
-      expect(e.message).to.contain('GCLX: Nin tai tan xin le');
+      expect(e.message).to.contain('GCLX: Zui duo lia');
     }
 
     // mint 1 out of 2
-    await hardhatToken
-      .connect(allowAccount)
-      .allowlistMintMerkleProof(1, proof, {
-        value: ethers.utils.parseEther('0.01'),
-      });
+    await hardhatToken.connect(allowAccount).allowlistMint(1, proof, {
+      value: ethers.utils.parseEther('0.01'),
+    });
 
     // can still mint since there is still 1 quota left
-    await hardhatToken
-      .connect(allowAccount)
-      .allowlistMintMerkleProof(1, proof, {
-        value: ethers.utils.parseEther('0.01'),
-      });
+    await hardhatToken.connect(allowAccount).allowlistMint(1, proof, {
+      value: ethers.utils.parseEther('0.01'),
+    });
   });
 
-  it('allowlistMintMerkleProof Test #4 - user can call both allowlistMintMerkleProof and mint functions if status=started', async function () {
+  it('allowlistMintTest #4 - user can mint from either allowlist or public and with separate quota for each', async function () {
     await hardhatToken.setStatus(1);
     allowAccount = allowlistAccounts[2];
     proof = merkleTree.getHexProof(keccak256(allowAccount.address));
@@ -103,42 +93,55 @@ describe('contract test - allowList with MerkleProof', function () {
       expect(e.message).to.contain('GCLX: Zui duo lia');
     }
 
+    await hardhatToken.setStatus(1);
+
     // the user can still call allowlistmint() as she is in the allowlist with quota of 2
-    await hardhatToken
-      .connect(allowAccount)
-      .allowlistMintMerkleProof(2, proof, {
-        value: ethers.utils.parseEther('0.02'),
-      });
+    await hardhatToken.connect(allowAccount).allowlistMint(2, proof, {
+      value: ethers.utils.parseEther('0.02'),
+    });
 
     // the next call should fail as the user has used up her allowlist quota
     try {
       expect(
-        await hardhatToken
-          .connect(allowAccount)
-          .allowlistMintMerkleProof(1, proof, {
-            value: ethers.utils.parseEther('0.04'),
-          })
+        await hardhatToken.connect(allowAccount).allowlistMint(1, proof, {
+          value: ethers.utils.parseEther('0.04'),
+        })
       ).to.throw();
     } catch (e) {
-      expect(e.message).to.contain('GCLX: Ling guo le');
+      expect(e.message).to.contain('GCLX: Zui duo lia(bai ming dan)');
     }
   });
 
-  it('allowlistMintMerkleProof Test #5 - not enought ETH ', async function () {
+  it('allowlistMintTest #5 - not enought ETH ', async function () {
+    await hardhatToken.setStatus(1);
     allowAccount = allowlistAccounts[3];
     proof = merkleTree.getHexProof(keccak256(allowAccount.address));
 
     // the next call should fail as the user tries to mint below the PRICE
     try {
       expect(
-        await hardhatToken
-          .connect(allowAccount)
-          .allowlistMintMerkleProof(2, proof, {
-            value: ethers.utils.parseEther('0.01'),
-          })
+        await hardhatToken.connect(allowAccount).allowlistMint(2, proof, {
+          value: ethers.utils.parseEther('0.004'),
+        })
       ).to.throw();
     } catch (e) {
       expect(e.message).to.contain('GCLX: Mei duo gei ETH.');
     }
+  });
+
+  it('llowlistMintTest #6 - refund if giving more ETH than the price', async function () {
+    await hardhatToken.setStatus(1);
+    allowAccount = allowlistAccounts[4];
+    proof = merkleTree.getHexProof(keccak256(allowAccount.address));
+    const initialOwnerBalance = await hardhatToken
+      .connect(allowAccount)
+      .balanceOf(allowAccount.address);
+    await hardhatToken
+      .connect(allowAccount)
+      .mint(1, { value: ethers.utils.parseEther('10.01') });
+    const finalOwnerBalance = await hardhatToken
+      .connect(allowAccount)
+      .balanceOf(allowAccount.address);
+    expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(-1));
   });
 });
